ad36a18f21d2d20f947714ddbd3b2175bf54a5b7
Jordan <jordandoan@hotmail.com>
2019-10-04 08:35:08 -0700

    Added necessary packages and scripts

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+const port = 5000;
+
+server.listen(port, () => {
+  console.log("Server is online");
+});
